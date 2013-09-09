dc313be7001ebbb987de0cbc45ddd67f0879bc77
Karl Podger <karl@wednesdayagency.com>
2013-09-09 12:01:46 +0100

    Specify dotfiles for OS-X only.

--- modules/wednesday/manifests/environment.pp
+++ modules/wednesday/manifests/environment.pp
@@ -2,6 +2,6 @@ class wednesday::environment {
   include wednesday::os::mac
   include wednesday::packages::mac
   include wednesday::applications::mac
-  include wednesday::dotfiles
+  include wednesday::dotfiles::mac
   include wednesday::projects
 }
