e11eadd0c80dce0a4494dd63089a249e416d932f
Karl Podger <karl@wednesdayagency.com>
2013-09-23 09:28:04 +0100

    Add server settings.

--- modules/projects/manifests/jbrandjeans.pp
+++ modules/projects/manifests/jbrandjeans.pp
@@ -3,6 +3,8 @@ class projects::jbrandjeans {
   boxen::project { 'jbrandjeans':
     dir           => "${boxen::config::srcdir}/sites/jbrandjeans.com/deploy",
     mysql         => true,
+    redis         => true,
+    nginx         => true,
     source        => 'wednesdaylondon/jbrandjeans.com'
   }
 }
