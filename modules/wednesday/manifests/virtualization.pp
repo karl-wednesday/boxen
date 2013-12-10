ba00730c6d9600f10e9867c3b6a2ec8473f22f67
Karl Podger <karl@wednesdayagency.com>
2013-12-10 03:30:53 +0000

    Configure vagrant.

--- modules/wednesday/manifests/virtualization.pp
+++ modules/wednesday/manifests/virtualization.pp
@@ -1,7 +1,20 @@
 class wednesday::virtualization::mac {
   include vagrant
 
+  $home = "/Users/${::boxen_user}"
+  $vagrant_dir = "${boxen::config::srcdir}/vagrant"
+
   vagrant::box { 'precise64/virtualbox':
     source => 'http://files.vagrantup.com/precise64.box'
   }
+
+  repository { $vagrant_dir:
+    source => 'wednesdaylondon/vagrant'
+  }
+
+	exec { "VirtualBox_reset":
+		command => "VirtualBox restart",
+		path    => "/Library/StartupItems/VirtualBox/",
+		user    => root
+	}
 }
