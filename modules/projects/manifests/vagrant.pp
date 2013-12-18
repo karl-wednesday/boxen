19792d718de37fa9119c9123782399223ea66606
Karl Podger <karl@wednesdayagency.com>
2013-12-18 14:41:08 +0000

    Set sites directory ownership to vagrant user.

--- modules/projects/manifests/vagrant.pp
+++ modules/projects/manifests/vagrant.pp
@@ -1,7 +1,18 @@
 class projects::vagrant {
 
-  boxen::project { 'vagrant':
-    dir             => "${boxen::config::srcdir}/sites",
+  $sites_dir = "${boxen::config::srcdir}/sites"
+
+  boxen::project { "vagrant":
+    dir           => "${sites_dir}",
+  }
+
+  file { "${sites_dir}":
+    ensure => directory,
+    recurse => true,
+    ignore => "{.git,.svn,.DS_Store}",
+    owner => "vagrant",
+    group => "vagrant"
+    # mode => 777
   }
 
   host { 'vagrant':
