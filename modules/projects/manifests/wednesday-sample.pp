c3cdded5b99cef53603f735b4d2ed632c2b397bb
Karl Podger <karl@wednesdayagency.com>
2014-01-06 14:36:36 +0000

    Puppet require checks.

--- modules/projects/manifests/wednesday-sample.pp
+++ modules/projects/manifests/wednesday-sample.pp
@@ -6,14 +6,6 @@ class projects::wednesday-sample {
   #include php::5_4_11
   include php::composer
 
-  $project_dir = "${boxen::config::srcdir}/sites/wednesday-sample"
-
-  boxen::project { "wednesday-sample":
-    dir           => "${project_dir}",
-    mysql         => true,
-    source        => "karl-wednesday/wednesday-sample"
-  }
-
   host { "wednesday-sample.host":
     ip => "192.168.56.101",
     host_aliases => "www.wednesday-sample.host",
@@ -24,35 +16,66 @@ class projects::wednesday-sample {
     host_aliases => "dbv.wednesday-sample.host",
   }
 
-  #check_mode { "${project_dir}/build/public/wp-content":
-  #  mode => 777
-  #}
+  $project_dir = "${boxen::config::srcdir}/sites/wednesday-sample"
+  $wordpress_dir = "${project_dir}/build/public"
+  $dbv_dir = "${project_dir}/dbv"
 
-  file { "${project_dir}/build/public/wp-content":
-    ensure => directory,
-    recurse => true,
-    ignore => ".git",
-    #owner => $puppet::params::puppet_user,
-    #group => $puppet::params::puppet_group,
-    mode => 777
+  boxen::project { "wednesday-sample":
+    dir           => "${project_dir}",
+    mysql         => true,
+    source        => "karl-wednesday/wednesday-sample"
   }
-
-  file { "${project_dir}/sql/data":
-    ensure => directory,
-    recurse => true,
-    ignore => ".git",
-    #owner => $puppet::params::puppet_user,
-    #group => $puppet::params::puppet_group,
-    mode => 777
+  
+  repository {
+  "${wordpress_dir}":
+    source   => "WordPress/WordPress", #short hand for github repos
+    provider => "git",
+    require => Repository[$project_dir];
   }
-
-  file { "${project_dir}/dbv/config.php":
-    source  => "${project_dir}/config/dbv/config.php",
-    require => Repository[$project_dir]
-  }
-
-  file { "${project_dir}/build/public/wp-config.php":
+  
+  file { "${wordpress_dir}/wp-config.php":
+    #ensure => directory,
     source  => "${project_dir}/config/wordpress/wp-config.php",
+    require => [
+      Repository[$project_dir],
+      Repository[$wordpress_dir]
+    ]
+  }
+  
+  file { "${wordpress_dir}/wp-content":
+    #ensure => directory,
+    recurse => true,
+    ignore => ".git",
+    owner => $puppet::params::puppet_user,
+    group => $puppet::params::puppet_group,
+    mode => 775,
+    require => Repository[$wordpress_dir]
+  }
+
+  repository {
+  "${dbv_dir}":
+    source   => "victorstanciu/dbv", #short hand for github repos
+    provider => "git",
+    require => Repository[$project_dir];
+  }
+  
+  file { "${dbv_dir}/config.php":
+    #ensure => directory,
+    source  => "${project_dir}/config/dbv/config.php",
+    require => [
+      Repository[$project_dir],
+      Repository[$dbv_dir]
+    ]
+  }
+  
+  file { "${project_dir}/sql/data":
+    #ensure => directory,
+    recurse => true,
+    ignore => ".git",
+    owner => $puppet::params::puppet_user,
+    group => $puppet::params::puppet_group,
+    mode => 775,
     require => Repository[$project_dir]
   }
+
 }
