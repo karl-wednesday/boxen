f687bc7605ccbaf9172eb300f50f5b92b676a041
Karl Podger <karl@wednesdayagency.com>
2014-01-10 01:26:51 +0000

    Upgrade Node and npm packages.

--- modules/wednesday/manifests/packages.pp
+++ modules/wednesday/manifests/packages.pp
@@ -13,8 +13,11 @@ class wednesday::packages::mac {
 	#include memcached::lib
 	include mongodb
 	include mysql
+	# include nodejs::v0_10_1
 	include nginx
+	#include ntfs_3g
 	#include nvm
+	#include osxfuse
 	include pcre
 	include phantomjs
 	include php
@@ -30,59 +33,69 @@ class wednesday::packages::mac {
 	#include wkhtmltopdf
 	include zeromq
 
+	class { 'nodejs::global':
+		version => 'v0.10.21'
+	}
 
 	class { 'ruby::global':
 		version => '2.0.0'
-	}
-
-	class { 'nodejs::global':
-		version => 'v0.10.21'
 	}
 
 	package { 'casperjs':
 		install_options => ['--devel']
 	}
 
+	# package { 'phantomjs':
+	# }
+
+	package { 'slimerjs':
+	}
+
 	nodejs::module { 'bower':
 		module  => 'bower',
-		node_version => 'v0.10.3'
+		node_version => 'v0.10.21'
 	}
 
 	nodejs::module { 'grunt-cli':
 		module  => 'grunt-cli',
-		node_version => 'v0.10.3'
+		node_version => 'v0.10.21'
 	}
 
 	nodejs::module { 'less':
 		module  => 'less',
-		node_version => 'v0.10.3'
+		node_version => 'v0.10.21'
 	}
 
 	nodejs::module { 'jshint':
 		module  => 'jshint',
-		node_version => 'v0.10.3'
+		node_version => 'v0.10.21'
 	}
 
 	nodejs::module { 'recess':
 		module  => 'recess',
-		node_version => 'v0.10.3'
+		node_version => 'v0.10.21'
+	}
+
+	nodejs::module { 'sails':
+		module  => 'sails',
+		node_version => 'v0.10.21'
 	}
 
 	nodejs::module { 'yo':
 		module  => 'yo',
-		node_version => 'v0.10.3'
-	}
-
-	ruby::gem { 'sass for 2.0.0':
-		gem     => 'sass',
-		ruby    => '2.0.0',
-		version => '~> 3.2.13'
+		node_version => 'v0.10.21'
 	}
 
 	ruby::gem { 'compass for 2.0.0':
 		gem     => 'compass',
 		ruby    => '2.0.0',
 		version => '~> 0.12.2'
+	}
+
+	ruby::gem { 'sass for 2.0.0':
+		gem     => 'sass',
+		ruby    => '2.0.0',
+		version => '~> 3.2.13'
 	}
 
 	# class gource {
