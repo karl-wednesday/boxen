51e4c237c285d20e5f7312b91e0e8b1f870fcf0f
Karl Podger <karl@wednesdayagency.com>
2014-05-28 15:37:05 +0100

    Disable exec commands that Puppet is identifying as duplicate.

--- modules/projects/manifests/kentandcurwen.pp
+++ modules/projects/manifests/kentandcurwen.pp
@@ -3,33 +3,34 @@ class projects::kentandcurwen {
 	boxen::project { 'kentandcurwen':
 		dir           => "${boxen::config::srcdir}/sites/kentandcurwen.com/deploy",
 		mysql         => true,
+		nginx         => false,
 		source        => 'wednesdaylondon/kentcurwen'
 	}
 
 	host { 'kentandcurwen.com.host':
 		ip => '192.168.56.101',
-		host_aliases => 'www.kentandcurwen.com.host'
+		host_aliases => ['www.kentandcurwen.com.host', 'kentcurwen.dev']
 	}
 
-	exec { "/opt/boxen/nodenv/shims/npm install":
-		cwd     => "${project_dir}",
-		creates => "${project_dir}/node_modules",
-		onlyif => ["/bin/test -d ${project_dir}"]
-	}
+	# exec { "/opt/boxen/nodenv/shims/npm install":
+	# 	cwd     => "${project_dir}",
+	# 	creates => "${project_dir}/node_modules",
+	# 	onlyif => ["/bin/test -d ${project_dir}"]
+	# }
 
-	exec { "/opt/boxen/nodenv/shims/npm update":
-		cwd     => "${project_dir}",
-		onlyif => ["/bin/test -d ${project_dir}", "/bin/test -d ${project_dir}/node_modules"]
-	}
+	# exec { "/opt/boxen/nodenv/shims/npm update":
+	# 	cwd     => "${project_dir}",
+	# 	onlyif => ["/bin/test -d ${project_dir}", "/bin/test -d ${project_dir}/node_modules"]
+	# }
 
-	exec { "/opt/boxen/nodenv/shims/bower install":
-		cwd     => "${project_dir}",
-		creates => "${project_dir}/node_modules",
-		onlyif => ["/bin/test -d ${project_dir}"]
-	}
+	# exec { "/opt/boxen/nodenv/shims/bower install":
+	# 	cwd     => "${project_dir}",
+	# 	creates => "${project_dir}/node_modules",
+	# 	onlyif => ["/bin/test -d ${project_dir}"]
+	# }
 
-	exec { "/opt/boxen/nodenv/shims/bower update":
-		cwd     => "${project_dir}",
-		onlyif => ["/bin/test -d ${project_dir}", "/bin/test -d ${project_dir}/bower_components"]
-	}
+	# exec { "/opt/boxen/nodenv/shims/bower update":
+	# 	cwd     => "${project_dir}",
+	# 	onlyif => ["/bin/test -d ${project_dir}", "/bin/test -d ${project_dir}/bower_components"]
+	# }
 }
--- modules/projects/manifests/swarovskigroup.pp
+++ modules/projects/manifests/swarovskigroup.pp
@@ -2,41 +2,37 @@ class projects::swarovskigroup {
 
 	$project_dir = "${boxen::config::srcdir}/sites/swarovskigroup.com/deploy"
 
-  boxen::project { 'swarovskigroup':
-    dir           => "${project_dir}",
-    mysql         => true,
-    source        => 'wednesdaylondon/swarovskigroup.com'
-  }
-
-  host { 'swarovskigroup.com.host':
-    ip => '192.168.56.101',
-    host_aliases => 'www.swarovskigroup.com.host',
-  }
-
-	exec { "/opt/boxen/nodenv/shims/npm install":
-	# exec { "npm install":
-		cwd     => "${project_dir}",
-		# path    => ["/opt/boxen/nodenv/shims"],
-		creates => "${project_dir}/node_modules",
-		onlyif => ["/bin/test -d ${project_dir}"]
-		# onlyif => ["/bin/test -d ${project_dir}", "/bin/test ! -d ${project_dir}/node_modules"]
+	boxen::project { 'swarovskigroup':
+		dir           => "${project_dir}",
+		mysql         => false,
+		nginx         => false,
+		source        => 'wednesdaylondon/swarovskigroup.com'
 	}
 
-	exec { "/opt/boxen/nodenv/shims/npm update":
-	# exec { "npm update":
-		cwd     => "${project_dir}",
-		# path    => ["/opt/boxen/nodenv/shims"],
-		onlyif => ["/bin/test -d ${project_dir}", "/bin/test -d ${project_dir}/node_modules"]
+	host { 'swarovskigroup.com.host':
+		ip => '192.168.56.101',
+		host_aliases => 'www.swarovskigroup.com.host',
 	}
 
-	exec { "/opt/boxen/nodenv/shims/bower install":
-		cwd     => "${project_dir}",
-		creates => "${project_dir}/node_modules",
-		onlyif => ["/bin/test -d ${project_dir}"]
-	}
+	# exec { "/opt/boxen/nodenv/shims/npm install":
+	# 	cwd     => "${project_dir}",
+	# 	creates => "${project_dir}/node_modules",
+	# 	onlyif => ["/bin/test -d ${project_dir}"]
+	# }
 
-	exec { "/opt/boxen/nodenv/shims/bower update":
-		cwd     => "${project_dir}",
-		onlyif => ["/bin/test -d ${project_dir}", "/bin/test -d ${project_dir}/bower_components"]
-	}
+	# exec { "/opt/boxen/nodenv/shims/npm update":
+	# 	cwd     => "${project_dir}",
+	# 	onlyif => ["/bin/test -d ${project_dir}", "/bin/test -d ${project_dir}/node_modules"]
+	# }
+
+	# exec { "/opt/boxen/nodenv/shims/bower install":
+	# 	cwd     => "${project_dir}",
+	# 	creates => "${project_dir}/node_modules",
+	# 	onlyif => ["/bin/test -d ${project_dir}"]
+	# }
+
+	# exec { "/opt/boxen/nodenv/shims/bower update":
+	# 	cwd     => "${project_dir}",
+	# 	onlyif => ["/bin/test -d ${project_dir}", "/bin/test -d ${project_dir}/bower_components"]
+	# }
 }
