04207306a39d76259f53f1f79d38c8336c31ad92
Karl Podger <karl@wednesdayagency.com>
2014-06-07 13:43:25 +0100

    Define git user on project.

--- modules/projects/manifests/cookadlib.pp
+++ modules/projects/manifests/cookadlib.pp
@@ -1,24 +1,25 @@
 class projects::cookadlib {
 
 	include git
-  include mongodb
+  # include mongodb
 
-	$project_dir = "${boxen::config::srcdir}/sites/cookadlib.com"
-
-	git::config::local { "${project_dir}":
-		ensure => present,
-		#repo   => "${project_dir}",
-		key    => "user.email",
-		value  => "karl.podger@primeordinal.com"
-	}
+	$project_dir = "${boxen::config::srcdir}/sites/cookadlib.com/deploy"
 
   boxen::project { "cookadlib":
     dir           => "${project_dir}",
-    mongodb       => true,
-    mysql         => true,
+    mongodb       => false,
+    mysql         => false,
     phantomjs     => false,
-    redis         => true,
+    redis         => false,
     source        => "cookadlib/cookadlib.com"
+  }
+
+  git::config::local { $project_dir:
+    ensure => present,
+    repo   => $project_dir,
+    require => Repository[$project_dir],
+    key    => "user.email",
+    value  => "karl.podger@primeordinal.com"
   }
 
   host { "cookadlib.com.host":
