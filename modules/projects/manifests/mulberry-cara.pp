a86d345099c6f58e6960ffe0e08242b0cfdfcf64
Karl Podger <karl@wednesdayagency.com>
2014-08-28 10:37:25 +0100

    Add mulberry-cara project manifest.

---
+++ modules/projects/manifests/mulberry-cara.pp
@@ -0,0 +1,14 @@
+class projects::mulberry {
+
+  boxen::project { 'mulberry':
+    dir           => "${boxen::config::srcdir}/sites/mulberry-cara/deploy",
+    mysql         => false,
+    nginx         => false,
+    source        => 'wednesdaylondon/mulberry-cara'
+  }
+
+  host { 'mulberry-cara.host':
+    ip => '192.168.56.101',
+    host_aliases => 'www.mulberry-cara.host',
+  }
+}
